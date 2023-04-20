8dcb242bd11f37ca69234e5d439f802c722d8164
Sean <95305272+seanscodes@users.noreply.github.com>
2023-04-20 19:47:36 +0200

    Create itwhitelist.lua

---
+++ whitelists/itwhitelist.lua
@@ -0,0 +1,8 @@
+2910453191, --// tavlnt: Head Of IT & IT Overseer
+647611724, --// mbeveridge | Deputy IT Head
+1346061799, --// steven | IT Manager
+1152635671, --// superior chairwoman -nev // dont remove me k thx
+3429474367, --// Chairperson -Brooke
+3469207977, --// viben
+3244213171, --// soft_koala3
+967034165, --// scripted_connor
